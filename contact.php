42cf779f400b89a17235f5246e30f22cb085c8f1
MartineBoulenger <75432239+MartineBoulenger@users.noreply.github.com>
2022-05-04 09:40:37 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page de contact d'Arlea</title>
+    <meta content="ARLEA est une jeune entreprise située sur le Bassin d'Arcachon. Créé  au printemps 2020, Arlea est en contact avec les professionnels qui souhaitent une tenue adaptée à leurs besoins." name="description">
+    <meta content="ARLEA est une jeune entreprise située sur le Bassin d'Arcachon. Simplifier les processus de personnalisation textile ainsi que satisfaire nos clients sont des valeurs qui nous tiennent particulièrement à cœur. 
+        Afin de pouvoir s'adapter à chacune de vos demandes, nous vous proposons une vaste gamme de textiles et de couleurs.  " name="keywords">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/templatemo-style.css">
+    <link rel="stylesheet" href="css/style.css">
+<!--
+    
+TemplateMo 556 Catalog-Z
+
+https://templatemo.com/tm-556-catalog-z
+
+-->
+</head>
+<body>
+    <!-- Page Loader -->
+    <div id="loader-wrapper">
+        <div id="loader"></div>
+
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+
+    </div>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <img src="img/logo.jpg" alt="Image" class="img-fluide">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="menu">
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-3 active" aria-current="page" href="index.html">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-1" href="#">Femmes</a>
+                        <ul class="sub-menu">
+                            <li>
+                                <a href="hautfemmes.html">Les hauts</a></li>
+                            <li><a href="basfemmes.html">Les bas</a></li>
+                            <li><a href="accessoirefemmes.html">Accessoires</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-1" href="#">Hommes</a>
+                        <ul class="sub-menu">
+                        <li>
+                            <a href="hauthommes.html">Les hauts</a></li>
+                            <li><a href="bashommes.html">Les bas</a></li>
+                            <li><a href="accessoirehommes.html">Accessoires</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link nav-link-1" href="#">Enfants</a>
+                        <ul class="sub-menu">
+                            <li>
+                            <a href="hautenfants.html">Les hauts</a></li>
+                            <li><a href="basenfants.html">Les bas</a></li>
+                            <li><a href="accessoireenfants.html">Accessoires</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link nav-link-4" href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="img/hero.jpg"></div>
+
+    <div class="container-fluid tm-mt-60">
+        <div class="row tm-mb-50">
+            <div class="col-lg-4 col-12 mb-5">
+                <h2 class="tm-text-primary mb-5">Contactez nous</h2>
+
+                <?php
+                if(
+                    !empty($_POST['mail']) && filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)&&
+                    !empty($_POST['name']) &&
+                    !empty($_POST['message'])
+                ){
+    
+                    $headers = 'From: ' . $_POST['mail'] . "\r\n" .
+                    'Reply-To: ' . $_POST['mail'] . "\r\n" .
+                    'X-Mailer: PHP/' . phpversion();
+                    mail('contact@arlea-vetements.fr', 'Formulaire de contact', $_POST['message'], $headers);
+                }elseif(!empty($_POST)){
+                    echo '<div class="alert alert-danger">Vous n\'avez pas rempli tous les champs</div>';
+                }
+                if(empty($_post)){
+                    $_POST = array(
+                        'mail' => '',
+                        'name' => '',
+                        'message' => ''
+                    );
+                }
+                ?>
+
+
+                <form id="contact-form" action="" method="POST" class="tm-contact-form mx-auto">
+                    <div class="form-group">
+                        <input type="text" name="name" class="form-control rounded-0" placeholder="Nom" required />
+                    </div>
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control rounded-0" placeholder="Email" required />
+                    </div>
+                    <div class="form-group">
+                        <select class="form-control" id="contact-select" name="inquiry">
+                            <option value="-">Sujet</option>
+                            <option value="sales">Plus d'informations</option>
+                            <option value="creative">Passer commande</option>
+
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <textarea rows="8" name="message" class="form-control rounded-0" placeholder="Message" required=></textarea>
+                    </div>
+
+                    <div class="form-group tm-text-right">
+                        <button type="submit" class="btn btn-primary">Envoyer</button>
+                    </div>
+                </form>                
+            </div>
+            <div class="col-lg-4 col-12 mb-5">
+                <div class="tm-address-col">
+                    <h2 class="tm-text-primary mb-5">Où nous trouver</h2>
+                    <address class="tm-text-gray tm-mb-50">
+                    8 avenue de la libération <br>
+                    33138 Lanton
+                    </address>
+                    <ul class="tm-contacts">
+                        <li>
+                            <a href="#" class="tm-text-gray">
+                                <i class="fas fa-envelope"></i>
+                                Email: contact@arlea-vetements.fr
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" class="tm-text-gray">
+                                <i class="fas fa-phone"></i>
+                                Tel: +33 6.77.79.86.61 
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.arlea-vetements.fr/" class="tm-text-gray">
+                                <i class="fas fa-globe"></i>
+                                https://www.arlea-vetements.fr/
+                            </a>
+                        </li>
+                    </ul>
+                </div>                
+            </div>
+            <div class="col-lg-4 col-12">
+                <h2 class="tm-text-primary mb-5">Nous localiser</h2>
+                <!-- Map -->
+                <div class="mapouter mb-4">
+                    <div class="gmap-canvas">
+                        <iframe width="100%" height="520" id="gmap-canvas"
+                            src=<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2835.8850520385586!2d-1.0359500841395002!3d44.701545692025796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54bd641403b9fb%3A0xcf75a40f4fcdcb82!2s8%20Av.%20de%20la%20Lib%C3%A9ration%2C%2033138%20Lanton!5e0!3m2!1sfr!2sfr!4v1651645413012!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                    </div>
+                </div>               
+            </div>
+        </div>
+    </div> <!-- container-fluid, tm-container-content -->
+
+    <footer class="tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer">
+        <div class="container-fluid tm-container-small">
+            <div class="row">
+                <div class="col-lg-6 col-md-12 col-12 px-5 mb-5">
+                    <h3 class="tm-text-primary mb-4 tm-footer-title">Pour passer commande en ligne</h3>
+                    <p>Arlea <a rel="sponsored" href="https://www.arlea-vetements.fr/">Boutique en ligne</a> Créé  au printemps 2020. Arlea
+                        
+                        est désormais en contact régulier avec des professionnels de la santé, avec le secteur socio médical, ainsi que tous les autres professionnels qui souhaitent une tenue adaptée à leurs besoins.</p>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
+                    <h3 class="tm-text-primary mb-4 tm-footer-title">Liens utiles</h3>
+                    <ul class="tm-footer-links pl-0">
+                        <li><a href="https://www.arlea-vetements.fr/">La boutique en ligne</a></li>
+                        <li><a href="mentionslegales.html">Mentions légales</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="https://creationwebdesignboulenger.com">Contacter le webmaster</a></li>
+
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 col-12 px-5 mb-5">
+                    <h3 class="tm-text-primary mb-4 tm-footer-title">Suivez nous !</h3>
+
+                    <ul class="tm-social-links d-flex justify-content-end pl-0 mb-5">
+                        <li class="mb-2"><a href="https://m.facebook.com/arleatextile/"><i class="fab fa-facebook"></i></a></li>
+                        <li class="mb-2"><a href="https://instagram.com/arlea_textile?igshid=YmMyMTA2M2Y="><i class="fab fa-instagram"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-md-7 col-12 px-5 mb-3">
+                    Copyright 2022 Arlea. All rights reserved.
+                </div>
+                <div class="col-lg-4 col-md-5 col-12 px-5 text-right">
+                    Designed by <a href="https://creationwebdesignboulenger.com" class="tm-text-gray" rel="sponsored" target="_parent">Boulenger Martine</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="js/plugins.js"></script>
+    <script>
+        $(window).on("load", function() {
+            $('body').addClass('loaded');
+        });
+    </script>
+</body>
+</html>
